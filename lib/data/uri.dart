25094772fc753c0f3ccb958728594751759ede38
thegreatcode1 <suhaibpa72@gmail.com>
2023-09-12 13:55:39 +0530

    package added

---
+++ lib/data/uri.dart
@@ -0,0 +1,9 @@
+class Url {
+  
+  String baseurl ="http://127.0.0.1:3000";
+
+  String createnote="/note/create";
+  String updatenote = "/note/update";
+  String getnote = "/note/getAll";
+  String deletenote = "/note/delete/{id}";
+}
